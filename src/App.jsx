04b29a6f4a6b882791fb437747c2d8ae4b778ee5
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-07 13:18:36 +0530

    React-router setup for routing and Home Page routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./Pages/HomePage";
 
 function App() {
-  return <>Hello Team Uniqaya !</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
