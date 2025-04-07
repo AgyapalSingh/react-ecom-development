ef68be8a12813c440293a2417713fab9c74a7efe
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-07 13:13:20 +0530

    Layout File created with helmet for SEO

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const Layout = ({
+  children,
+  title = "Uniqaya Lifestyle - Clean and Functional Skincare",
+  description = "Uniqaya's belief is that each and every God's creation is unique and we are here to inculcate healthy skin care habits with plant based ingredients to restore the beauty in you. Bridging nature and science with over 40 years of experience in the pharmaceuticals industry, to create only what's best for the skin.",
+  keywords = "uniqaya, lifestyle, skincare, beauty, cosmetics, natural, organic",
+  author = "Agyapal Singh",
+}) => {
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
+
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
