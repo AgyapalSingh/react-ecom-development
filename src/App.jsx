5fee3c28864688d0ac3a1b1aa0b7d2a5f0a2689d
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-09 12:40:33 +0530

    Header Product Page routing done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./Layout/Header/Header";
 import ReviewsPage from "./Pages/HeaderPages/ReviewsPage";
 import AboutUsPage from "./Pages/HeaderPages/AboutUsPage";
 import FaqsPage from "./Pages/HeaderPages/FaqsPage";
+import ProductsPage from "./Pages/HeaderPages/ProductsPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/shop-all-products" element={<ProductsPage />} />
         <Route path="/top-reviews" element={<ReviewsPage />} />
         <Route path="/about-us" element={<AboutUsPage />} />
         <Route path="/faqs" element={<FaqsPage />} />
