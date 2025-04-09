fd96ff89cd7d44e4aca6eb39a9babdcc3fc9b7d2
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-09 12:28:45 +0530

    About-Us Page Routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import "./App.css";
 import HomePage from "./Pages/HomePage";
 import Header from "./Layout/Header/Header";
 import ReviewsPage from "./Pages/ReviewsPage";
+import AboutUsPage from "./Pages/AboutUsPage";
 
 function App() {
   return (
     <>
       <Header />
-
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/top-reviews" element={<ReviewsPage />} />
+        <Route path="/about-us" element={<AboutUsPage />} />
       </Routes>
     </>
   );
