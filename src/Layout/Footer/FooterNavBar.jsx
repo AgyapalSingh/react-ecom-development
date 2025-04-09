f2b0632236eee5c8f783b2eac5a05bb585292f47
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-09 14:12:24 +0530

    Footer Navbar 75% created and updated

--- src/Layout/Footer/FooterNavBar.jsx
+++ src/Layout/Footer/FooterNavBar.jsx
@@ -1,7 +1,71 @@
+import { NavLink } from "react-router-dom";
+
 const FooterNavBar = () => {
   return (
-    <div>FooterNavBar</div>
-  )
-}
+    <nav className="uniq-ag-footer-navbar">
+      <div className="uniq-ag-footer-navbar-menus">
+        <h1>Support</h1>
+        <menu>
+          <NavLink to="/about-us">
+            <li> About Us</li>
+          </NavLink>
+          <NavLink to="/contact-us">
+            <li>Contact Us</li>
+          </NavLink>
+          <NavLink to="/blogs">
+            <li>Blogs</li>
+          </NavLink>
+          <NavLink to="/faqs">
+            <li>FAQS</li>
+          </NavLink>
+          <NavLink to="https://uniqaya.com/apps/parcelpanel">
+            <li>TRACK ORDER</li>
+          </NavLink>
+        </menu>
+      </div>
 
-export default FooterNavBar
+      <div className="uniq-ag-footer-navbar-menus">
+        <h1>Policy Info</h1>
+        <menu>
+          <NavLink to="/disclaimer">
+            <li> Disclaimer</li>
+          </NavLink>
+          <NavLink to="/privacy-policy">
+            <li>Privacy Policy</li>
+          </NavLink>
+          <NavLink to="/terms-conditions">
+            <li>Terms & Conditions</li>
+          </NavLink>
+          <NavLink to="/shipping-policy">
+            <li>Shipping Policy</li>
+          </NavLink>
+          <NavLink to="/cancellation-refund-policy">
+            <li>Cancellation/Refund Policy</li>
+          </NavLink>
+        </menu>
+      </div>
+
+      <div className="uniq-ag-footer-navbar-menus">
+        <h1>Highlights</h1>
+        <menu>
+          <NavLink to="https://uniqayalifestyle.com/highlights/">
+            <li>Ingredient of the Month</li>
+          </NavLink>
+          <NavLink to="/offers">
+            <li>Offers</li>
+          </NavLink>
+          <NavLink to="/press">
+            <li>Press & Media</li>
+          </NavLink>
+          <NavLink to="/bethechange">
+            <li>C.H.A.N.G.E</li>
+          </NavLink>
+        </menu>
+      </div>
+
+      <div className="uniq-ag-footer-navbar-menus"></div>
+    </nav>
+  );
+};
+
+export default FooterNavBar;
