c4fc47a8799b3608d8efee3e4ba40f2d657a150f
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-09 14:51:01 +0530

    Contact Us page routing done

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AboutUsPage from "./Pages/HeaderPages/AboutUsPage";
 import FaqsPage from "./Pages/HeaderPages/FaqsPage";
 import ProductsPage from "./Pages/HeaderPages/ProductsPage";
 import Footer from "./Layout/Footer/Footer";
+import ContactUsPage from "./Pages/FooterPages.jsx/ContactUsPage";
 
 function App() {
   return (
@@ -14,10 +15,16 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+
+        {/* Header NavBar */}
         <Route path="/shop-all-products" element={<ProductsPage />} />
         <Route path="/top-reviews" element={<ReviewsPage />} />
         <Route path="/about-us" element={<AboutUsPage />} />
         <Route path="/faqs" element={<FaqsPage />} />
+
+        {/* Footer NavBar */}
+        <Route path="/contact-us" element={<ContactUsPage />} />
+
       </Routes>
       <Footer />
     </>
