77977d3f056b07740099a384b152ef0a510da452
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-11 14:31:41 +0530

    syncTouch Removed  to smooth scrolling for Touch Devices

--- src/Libs/Utils/SmoothScrolling.jsx
+++ src/Libs/Utils/SmoothScrolling.jsx
@@ -1,7 +1,7 @@
 import { ReactLenis } from "lenis/react";
 function SmoothScrolling({ children }) {
   return (
-    <ReactLenis root options={{ lerp: 0.5, duration: 2, syncTouch: true }}>
+    <ReactLenis root options={{ lerp: 0.5, duration: 2 }}>
       {children}
     </ReactLenis>
   );
