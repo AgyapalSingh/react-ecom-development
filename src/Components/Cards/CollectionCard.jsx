cf1f35fc465d18acf206ea99bdfd19f4a3287940
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-11 12:57:27 +0530

    AllCollections Card css updated

--- src/Components/Cards/CollectionCard.jsx
+++ src/Components/Cards/CollectionCard.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const CollectionCard = ({ collection }) => {
   return (
     <div key={collection.id} className="uniq-ag-collection-card">
-      <h2>{collection.title}</h2>
+      <div className="uniq-ag-collection-card-title">
+        <h2>{collection.title}</h2>
+      </div>
+      <div className="uniq-ag-collection-card-img">
+        <img
+          src={collection.image?.url || "default-image.jpg"}
+          alt={collection.image?.altText || collection.title}
+        />
+      </div>
     </div>
   );
 };
