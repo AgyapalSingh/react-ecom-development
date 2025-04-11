6e7a79a3c93244d50e41d4d4c04890e2cb646075
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-04-11 14:53:30 +0530

    Collection Loader setup

--- src/Collections/AllCollections.jsx
+++ src/Collections/AllCollections.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import { GET_COLLECTIONS } from "../Libs/Shopify/Queries";
 import shopifyApi from "../Libs/Shopify/ShopifyApi";
 import CollectionCard from "../Components/Cards/CollectionCard";
+import CollectionCardsLoader from "../Loaders/CollectionCardsLoader";
 
 const AllCollections = () => {
+  const [loading, setLoading] = useState(true);
   const [collections, setCollections] = useState([]);
   const fetchCollections = async () => {
     const query = { query: GET_COLLECTIONS };
@@ -13,6 +15,8 @@ const AllCollections = () => {
       setCollections(fetchedCollections);
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
   useEffect(() => {
@@ -20,17 +24,23 @@ const AllCollections = () => {
   }, []);
 
   return (
-    <div className="uniq-ag-all-collections">
-      <div className="uniq-ag-all-collections-header">
-        <h2>All Collections</h2>
-      </div>
+    <>
+      {loading ? (
+        <CollectionCardsLoader />
+      ) : (
+        <div className="uniq-ag-all-collections">
+          <div className="uniq-ag-all-collections-header">
+            <h2>All Collections</h2>
+          </div>
 
-      <div className="uniq-ag-all-collections-list">
-        {collections.map(({ node }) => (
-          <CollectionCard key={node.id} collection={node} />
-        ))}
-      </div>
-    </div>
+          <div className="uniq-ag-all-collections-list">
+            {collections.map(({ node }) => (
+              <CollectionCard key={node.id} collection={node} />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
